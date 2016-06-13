a0e8ce028ec2abd24ff34880d117d67c90498fcb
Maurits van Mastrigt <maurits@nerdieworks.nl>
2016-06-13 15:17:57 +0200

    Improve list script.

--- bin/list.sh
+++ bin/list.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-cat output.log | jq -r 'select(.type == "data") | (.time + "\t" + (.speeds.download | tostring) + "\t" + (.speeds.upload | tostring))' | sort -r
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cat "$DIR/../output.log" \
+	| jq -r 'select(.type == "data") | (.time + "\t" + (.speeds.download | tostring) + "\t" + (.speeds.upload | tostring))' \
+	| sort -r
