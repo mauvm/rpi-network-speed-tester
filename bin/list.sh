c49dc7dddc97ae4ebff8adf2d0ce8810f1e6c6a9
Maurits van Mastrigt <maurits@nerdieworks.nl>
2016-06-14 18:05:12 +0200

    Improve speed of list script.

--- bin/list.sh
+++ bin/list.sh
@@ -2,6 +2,5 @@
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-cat "$DIR/../output.log" \
-	| jq -r 'select(.type == "data") | (.time + "\t" + (.speeds.download | tostring) + "\t" + (.speeds.upload | tostring))' \
-	| sort -r
+tac "$DIR/../output.log" \
+	| jq -r 'select(.type == "data") | (.time + "\t" + (.speeds.download | tostring) + "\t" + (.speeds.upload | tostring))'
